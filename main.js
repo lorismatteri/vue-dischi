dc260dab4cfa450ebc9b0681534ffb6822a7793b
Loris Matteri <loris.matteri93@gmail.com>
2021-01-11 15:51:18 +0100

    Richiesta API e push in array

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-	//Code
+	let app = new Vue ({
+		el: '#app',
+		data: {
+			diskList: [],
+		},
+		mounted(){				
+			axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+			.then(response => {
+				const disk = response.data.response;
+				console.log(disk);
+				this.diskList.push(disk);
+			});	
+			
+			console.log(this.diskList);
+		}
+	})
 });
